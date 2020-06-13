486b3e9e076404f1f9285600c519272b5c9642e6
Paulo Mellin Gimenes <paulo.gimenes@runrun.it>
2020-06-13 12:22:11 -0300

    Charlinho's refactor

--- trabalho2/src/algorithms/datastructures/StackArrayTest.java
+++ trabalho2/src/algorithms/datastructures/StackArrayTest.java
@@ -7,16 +7,14 @@ class StackArrayTest {
 	private final StackArray stackArray = new StackArray();
 
 	@Test
-	void testPush() {
-		int firstValue = 1;
-		int secondValue = 2;
-		stackArray.push(firstValue);
-		assertEquals(1, stackArray.size());
-		assertEquals(firstValue, stackArray.peek());
-		stackArray.push(secondValue);
-		assertEquals(secondValue, stackArray.peek());
-		assertEquals(2, stackArray.size());
-		stackArray.makeEmpty();
+	void Push_singleValue() {
+		int value = 1;
+		stackArray.push(value);
+		assertEquals(value, stackArray.peek());
+	}
+
+	@Test
+	void Push_multipleValues() {
 		for (int i = 0; i < stackArray.getMaxSize(); i++) {
 			stackArray.push(i);
 		}
@@ -26,7 +24,17 @@ class StackArrayTest {
 	}
 
 	@Test
-	void testPop() {
+	void Push_outOffLimitValue() {
+		for (int i = 0; i < stackArray.getMaxSize(); i++) {
+			stackArray.push(i);
+		}
+		int outOffLimitValue = 65;
+		stackArray.push(outOffLimitValue);
+		assertEquals(outOffLimitValue, stackArray.peek());
+	}
+
+	@Test
+	void Pop_removedItemValue() {
 		int firstValue = 1;
 		int secondValue = 2;
 		stackArray.push(firstValue);
@@ -36,54 +44,86 @@ class StackArrayTest {
 	}
 
 	@Test
-	void testPeek() {
+	void Peek_emptyStack() {
 		assertEquals(-1, stackArray.peek());
+	}
+
+	@Test
+	void Peek_lastValue() {
 		int firstValue = 1;
 		int secondValue = 2;
 		int thirdValue = 243;
 		stackArray.push(firstValue);
-		assertEquals(firstValue, stackArray.peek());
 		stackArray.push(secondValue);
-		assertEquals(secondValue, stackArray.peek());
 		stackArray.push(thirdValue);
 		assertEquals(thirdValue, stackArray.peek());
+	}
+
+	@Test
+	void Peek_lastWithPreviousPopValue() {
+		int firstValue = 1;
+		int secondValue = 2;
+		int thirdValue = 243;
+		stackArray.push(firstValue);
+		stackArray.push(secondValue);
+		stackArray.push(thirdValue);
 		stackArray.pop();
 		assertEquals(secondValue, stackArray.peek());
 	}
 
 	@Test
-	void testIsEmpty() {
+	void IsEmpry_withNoAddition() {
 		assertEquals(true, stackArray.isEmpty());
+	}
+
+	@Test
+	void IsEmpty_withPush() {
 		stackArray.push(654);
 		assertEquals(false, stackArray.isEmpty());
+	}
+
+	@Test
+	void IsEmpty_withPushAndPop() {
+		stackArray.push(654);
 		stackArray.pop();
 		assertEquals(true, stackArray.isEmpty());
 	}
 
 	@Test
-	void testIsFull() {
+	void IsFull_noInteractions() {
+		assertEquals(false, stackArray.isFull());
+	}
+	
+	@Test
+	void IsFull_pushAllPositions() {
 		assertEquals(false, stackArray.isFull());
 		for (int i = 0; i < stackArray.getMaxSize(); i++) {
 			stackArray.push(i);
 		}
 		assertEquals(true, stackArray.isFull());
+	}
+	
+	@Test
+	void IsFull_pushAllPositionsAndPop() {
+		assertEquals(false, stackArray.isFull());
+		for (int i = 0; i < stackArray.getMaxSize(); i++) {
+			stackArray.push(i);
+		}
 		stackArray.pop();
 		assertEquals(false, stackArray.isFull());
 	}
 
 	@Test
-	void testSize() {
+	void testSize_afterPusshAllPositions() {
 		assertEquals(0, stackArray.size());
 		for (int i = 0; i < stackArray.getMaxSize(); i++) {
 			stackArray.push(i);
 		}
 		assertEquals(stackArray.getMaxSize(), stackArray.size());
-		stackArray.pop();
-		assertEquals(stackArray.getMaxSize() - 1, stackArray.size());
 	}
 
 	@Test
-	void testMakeEmpty() {
+	void MakeEmpty_afterPushAllPositions() {
 		for (int i = 0; i < stackArray.getMaxSize(); i++) {
 			stackArray.push(i);
 		}
